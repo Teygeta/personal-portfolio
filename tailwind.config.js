1a74a601f7a283e8ff44bd7bd24d741f24f3d205
teygeta <vitto.gioda@outlook.it>
2023-03-18 02:46:14 +0100

    first commit, developed home page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
+      'sulphur': ['Sulphur Point', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}

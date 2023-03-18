1c949df21bf911fdc3c276a23461ccb569b1e21a
teygeta <vitto.gioda@outlook.it>
2023-03-18 18:26:38 +0100

    dev. technology section

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,9 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200&family=Sulphur+Point:wght@400;700&display=swap" rel="stylesheet" />
-      </Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;600&family=Sulphur+Point:wght@400;700&display=swap" rel="stylesheet" />      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import Image from "next/image"
-export default function Home() {
+import { motion, useScroll, useTransform } from "framer-motion"
 
+export default function Home() {
   const homeImage = {
     objectFit: "cover",
     zIndex: "-1",
-    top: "50px",
   }
+
+  let { scrollYProgress } = useScroll()
+  let y = useTransform(scrollYProgress, [0, 2], ["0%", "-100%"]) //TODO capire se usare scrollY oppure scrollYProgress
+
   return (
     <>
-      <header>
+      <header className="h-screen">
         <div className="h-fit">
           <Image
             alt="home-bg"
@@ -24,22 +28,48 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <nav className="fixed flex ml-10 mt-3 gap-10 font-inter font-extralight text-[#BEDBD4] mix-blend-difference">
+        {/* TODO fare navbar con sfondo nero quando scrollo in su */}
+        <nav className="backdrop-blur-md z-20 fixed w-full flex pl-10 py-3 gap-10 font-inter font-extralight text-[#BEDBD4] mix-blend-difference">
           <div>Home</div>
           <div>Technology</div>
           <div>Portfolio</div>
           <div>About</div>
         </nav>
-        <div className="pt-72 ml-[15%] w-fit">
-          <p className="font-inter font-extralight text-2xl text-[#BEDBD4] mix-blend-difference">
+        <motion.div className="pt-72 ml-[15%] w-fit mix-blend-difference" style={{ y }}>
+          <p className="font-extralight text-2xl text-[#BEDBD4]">
             Hi, I&apos;m Vittorio.
           </p>
-          <h1 className="text-8xl text-[#d5ebe7] mix-blend-difference">
+          <h1 className="text-8xl font-sulphur text-[#d5ebe7] mix-blend-difference">
             FULL-STACK WEB <br />STUDENT <br />AND DEVELOPER
           </h1>
-        </div>
-
+        </motion.div>
       </header>
+      <motion.section style={{ y }} className="h-screen relative">
+        <div className="z-30 text-right absolute top-1/2 right-1/2 translate-x-1/2">
+          <div className="text-2xl font-extralight">
+            What I mainly use in my projects
+          </div>
+          <div className="custom-text-gradient-bg text-8xl italic font-semibold">
+            <div>NEXTJS</div>
+            <div>TAILWIND</div>
+            <div>EXPRESS</div>
+            <div>MYSQL</div>
+            <div>SUPABASE</div>
+            <div>FRAMER</div>
+          </div>
+        </div>
+      </motion.section>
+      <section className="portfolio-section h-screen">
+        <div className="">
+          <Image
+            alt="home-bg"
+            src="/unsplash_8xznAGy4HcY.png"
+            width={1440}
+            height={300}
+            quality={100}
+          />
+        </div>
+      </section>
     </>
   )
 }

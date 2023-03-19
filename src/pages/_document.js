cc3b0c2ffa2455e5e201852ac52cab50fbc9b7c6
teygeta <vitto.gioda@outlook.it>
2023-03-19 12:55:14 +0100

    dev. seo side

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,8 +4,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="title" content="Vittorio Gioda's portfolio" />
+        <meta name="description" content="Hi, I'm Vittorio. FULL-STACK WEB STUDENT AND DEVELOPER" />
+        <meta name="keywords" content="portfolio, personal, developer, student, full-stack, web-developer" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Vittorio Gioda" />
+        <meta name="robots" content="noindex,nofollow" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;600&family=Sulphur+Point:wght@400;700&display=swap" rel="stylesheet" />      </Head>
       <body>
         <Main />

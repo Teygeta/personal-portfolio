514543cdc2e089d998aad08070f90025df93e6f1
Teygeta <vitto.gioda@outlook.it>
2023-05-22 12:19:53 +0200

    add iubenda gdpr

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,13 @@
 export function Footer() {
+
+  const iubendaPrivacyDocumentation = `
+  <a href="https://www.iubenda.com/privacy-policy/21208842" class="font-semibold" title="Privacy">Privacy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+  `
+
+  const iubendaCookieDocumentation = `
+  <a href="https://www.iubenda.com/privacy-policy/21208842/cookie-policy" class="font-semibold" title="Cookie Policy">Cookie Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+  `
+
   return (
     <footer className="pb-10">
       <div className="border-t opacity-20 mb-10"></div>
@@ -6,6 +15,8 @@ export function Footer() {
         <p className="text-[16px]">Designed and built By Vittorio Gioda </p>
         <p className="text-[16px]">2023 - Turin, Italy</p>
         <p className="text-[16px]">P.IVA 12879240013</p>
+        <div class="my-2" dangerouslySetInnerHTML={{ __html: iubendaPrivacyDocumentation }}></div>
+        <div class="my-2" dangerouslySetInnerHTML={{ __html: iubendaCookieDocumentation }}></div>
       </div>
       {/* </div>
       </div>*/}
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,67 +1,75 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const iubendaImplementation = `
+<script type="text/javascript">
+var _iub = _iub || [];
+_iub.csConfiguration = {"askConsentAtCookiePolicyUpdate":true,"floatingPreferencesButtonDisplay":"bottom-right","perPurposeConsent":true,"siteId":3143084,"whitelabel":false,"cookiePolicyId":21208842,"lang":"it", "banner":{ "acceptButtonDisplay":true,"closeButtonRejects":true,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-bottom-center","zIndex":100 }};
+</script>
+<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+`
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         {/* HTML Meta Tags */}
-        <meta name="title" content="Vittorio Gioda Web Developer"/>
+        <meta name="title" content="Vittorio Gioda Web Developer" />
         <meta name="description"
-              content="Creo siti web ad alte performance, con un design accattivante ed una comunicazione efficace."/>
-        <meta name="keywords" content="portfolio, personal, developer, student, full-stack, web-developer"/>
+          content="Creo siti web ad alte performance, con un design accattivante ed una comunicazione efficace." />
+        <meta name="keywords" content="portfolio, personal, developer, student, full-stack, web-developer" />
         <meta name="keywords"
-              content="designer, work, development, restyling, design, sito web, sito, creare sito web, comprare sito web"/>
-        <meta name="robots" content="index, follow"/>
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
-        <meta name="language" content="Italian"/>
-        <meta name="author" content="Vittorio Gioda"/>
+          content="designer, work, development, restyling, design, sito web, sito, creare sito web, comprare sito web" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="Italian" />
+        <meta name="author" content="Vittorio Gioda" />
 
         {/* Google */}
-        <meta itemProp="name" content="Vittorio Gioda Web Developer"/>
+        <meta itemProp="name" content="Vittorio Gioda Web Developer" />
         <meta itemProp="description"
-              content="Creo siti web ad alte performance, con un design accattivante ed una comunicazione efficace."/>
+          content="Creo siti web ad alte performance, con un design accattivante ed una comunicazione efficace." />
         <meta itemProp="image"
-              content="http://www.vittoriogioda.com/_next/image?url=%2Funsplash_WUtT8cGRPog.png&w=3840&q=100"/>
+          content="http://www.vittoriogioda.com/_next/image?url=%2Funsplash_WUtT8cGRPog.png&w=3840&q=100" />
 
         {/* Facebook */}
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://www.vittoriogioda.com/"/>
-        <meta property="og:title" content="Vittorio Gioda Web Developer"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.vittoriogioda.com/" />
+        <meta property="og:title" content="Vittorio Gioda Web Developer" />
         <meta property="og:description"
-              content="Creo siti web ad alte performance, con un design accattivante ed una comunicazione efficace."/>
+          content="Creo siti web ad alte performance, con un design accattivante ed una comunicazione efficace." />
         <meta property="og:image"
-              content="http://www.vittoriogioda.com/_next/image?url=%2Funsplash_WUtT8cGRPog.png&w=3840&q=100"></meta>
+          content="http://www.vittoriogioda.com/_next/image?url=%2Funsplash_WUtT8cGRPog.png&w=3840&q=100"></meta>
 
         {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:title" content="Vittorio Gioda Web Developer"/>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Vittorio Gioda Web Developer" />
         <meta name="twitter:description"
-              content="Creo siti web ad alte performance, con un design accattivante ed una comunicazione efficace."/>
+          content="Creo siti web ad alte performance, con un design accattivante ed una comunicazione efficace." />
         <meta name="twitter:image"
-              content="http://www.vittoriogioda.com/_next/image?url=%2Funsplash_WUtT8cGRPog.png&w=3840&q=100"/>
+          content="http://www.vittoriogioda.com/_next/image?url=%2Funsplash_WUtT8cGRPog.png&w=3840&q=100" />
 
 
         {/* Icons */}
-        <link rel="shortcut icon" href="/favicons/favicon.ico"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
 
         {/* Apple */}
-        <meta name="apple-mobile-web-app-title" content="Gioda Web Developer"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
+        <meta name="apple-mobile-web-app-title" content="Gioda Web Developer" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
 
         {/* Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100;0,300;0,400;0,600;0,700;1,100;1,400;1,500;1,600&display=swap"
-          rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet"/>
-
+          rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet" />
       </Head>
       <body>
-      <Main/>
-      <NextScript/>
+        <Main />
+        <NextScript />
+        <div dangerouslySetInnerHTML={{ __html: iubendaImplementation }}></div>
       </body>
     </Html>
   )

a1dd8eed385e943ddd48c21c199fda3ed01378d3
Vittorio Gioda <98349901+Teygeta@users.noreply.github.com>
2023-07-06 11:28:05 +0200

    refractor: move paragraph in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,11 @@ export function Footer() {
     <footer className="pb-10">
       <div className="border-t opacity-20 mb-10"></div>
       <div className="text-center">
-        <p className="text-[16px]">Designed and built By Vittorio Gioda </p>
         <p className="text-[16px]">2023 - Turin, Italy</p>
         <p className="text-[16px]">P.IVA 12879240013</p>
         <div class="my-2" dangerouslySetInnerHTML={{ __html: iubendaPrivacyDocumentation }}></div>
         <div class="my-2" dangerouslySetInnerHTML={{ __html: iubendaCookieDocumentation }}></div>
+        <p className="text-[16px]">Designed and built By Vittorio Gioda </p>
       </div>
       {/* </div>
       </div>*/}
